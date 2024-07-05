b4d5cc6eb1784ba070d46591fa02dc5e2d3c5fd7
632-Jomar <jalarcon.632apps@gmail.com>
2024-07-05 23:56:29 +0800

    modify sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,20 +1,10 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    {{-- <a href="#" class="brand-link text-center">
-      <img src="{{ asset('assets/logos/logophil_circle.png') }}" alt="Cleanbreeze Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
-    </a> --}}
+    <a href="{{ route('quotations.index') }}" class="brand-link">
+        <img src="{{ asset('assets/logos/logophil_circle.png') }}" class="brand-image img-circle" style="opacity: .8">
+        <p class="brand-text font-weight-light m-0">Clean<span class="text-info">breeze</span></p>
+    </a>
 
     <div class="sidebar">
-        {{-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-            </div>
-
-            <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
-            </div>
-        </div> --}}
-
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
@@ -80,45 +70,3 @@
         </nav>
     </div>
 </aside>
-
-{{-- <div class="sidebar bg-primary" id="sidebar">
-    <div class="sidebar-inner slimscroll">
-        <div id="sidebar-menu" class="sidebar-menu">
-            <ul>
-                <li>
-                    <a class="text-light" href="{{ route('quotations.index') }}"><i class="fa fa-list"></i> <span>Quotations</span></a>
-                </li>
-
-                <li>
-                    <a class="text-light" href="{{ route('quotations.create') }}"><i class="fa fa-cart-plus"></i> <span>Create Quotation</span></a>
-                </li>
-
-                <li>
-                    <a class="text-light" href=""><i class="fa fa-user"></i> <span>Profile</span></a>
-                </li>
-
-                @if (auth()->user()->user_type_id == 1)
-                    <li>
-                        <a class="text-light" href="{{ route('accounts.index') }}"><i class="fa fa-id-badge"></i> <span>Accounts</span></a>
-                    </li>
-
-                    <li>
-                        <a class="text-light" href="{{ route('products.index') }}"><i class="fa fa-cart-plus"></i> <span>Products</span></a>
-                    </li>
-
-                    <li>
-                        <a class="text-light" href=""><i class="fa fa-file"></i> <span>Reports</span></a>
-                    </li>
-                @endif
-
-                <li>
-                    <a class="text-light" href=""><i class="fa fa-history"></i> <span>Activity Logs</span></a>
-                </li>
-
-                <li>
-                    <a class="text-light" href="#" class="logout_user"><i class="fa fa-sign-out"></i> <span>Logout</span></a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div> --}}
