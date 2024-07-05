eec12eecf7c01996a867f71a70a2eed9df8bd277
632-Jomar <jalarcon.632apps@gmail.com>
2024-07-05 23:29:22 +0800

    modify activityt logs

--- resources/views/activity-logs/index.blade.php
+++ resources/views/activity-logs/index.blade.php
@@ -29,7 +29,7 @@
                                     <td class="align-middle py-1">{{ $activityLog->entity_type }}</td>
                                     <td class="align-middle py-1" style="line-height: 110%">{{ $activityLog->created_at->format('Y, M d') }} <span class="text-xs">{{ $activityLog->created_at->format('(h:i a)') }}</span></td>
                                     <td class="align-middle py-1">{{ $activityLog->description }}</td>
-                                    <td class="align-middle py-1">{{ $activityLog->user->name }}</td>
+                                    <td class="align-middle py-1">{{ $activityLog->user->name ?? '' }}</td>
                                 </tr>
                             @empty
                                 <tr>
