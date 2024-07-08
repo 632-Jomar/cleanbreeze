d9d892b7919678b0940c947d857c2ca4550a7df6
Jomar <jalarcon.632apps@gmail.com>
2024-07-08 09:55:04 +0800

    add clear-cache route to server

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,11 @@ Route::middleware(['auth'])->group(function () {
         'activity-logs' => 'ActivityLogController'
     ]);
 });
+
+/** For clearing server-cache */
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('config:clear');
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('config:cache');
+    return 'DONE'; //Return anything
+});
