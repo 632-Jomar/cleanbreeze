316e665f2a66d9b4888d6f15a70434ebc5ad5c21
Jomar <jalarcon.632apps@gmail.com>
2024-07-15 09:34:31 +0800

    fix created_by

--- app/Http/Traits/HasCreatedBy.php
+++ app/Http/Traits/HasCreatedBy.php
@@ -7,7 +7,9 @@ trait HasCreatedBy
     public static function bootHasCreatedBy()
     {
         static::creating(function($model) {
-            $model->created_by = auth()->id() ?? $model->created_by;
+            if (is_null($model->created_by)) {
+                $model->created_by = auth()->id();
+            }
         });
     }
 }
